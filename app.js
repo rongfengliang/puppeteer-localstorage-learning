1ad36f2dda38daf1d53051a98d7c67656055be9c
rongfengliang <1141591465@qq.com>
2024-05-15 22:12:22 +0800

    add more

---
+++ app.js
@@ -0,0 +1,18 @@
+const puppeteer = require("puppeteer-core");
+(async () => {
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: "ws://localhost:3000",
+  });
+  const page = await browser.newPage();
+  await page.goto("http://nginx", { waitUntil: "networkidle2" });
+  await page.evaluate(() => {
+    localStorage.setItem("name", "demoapp");
+  });
+
+  let info = await page.evaluate(() => {
+    return localStorage.getItem("name");
+  });
+  console.log(info);
+  await page.close();
+  await browser.close();
+})();
